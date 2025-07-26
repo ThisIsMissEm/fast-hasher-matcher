4239a898a7cfc5fac19add39da24640e6088ccdc
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-07-26 16:21:57 +0200

    Add support for content-type guessing for remote media

--- app/routers/hashing.py
+++ app/routers/hashing.py
@@ -8,6 +8,8 @@ import logging
 import tempfile
 from pydantic_core import InitErrorDetails, PydanticCustomError
 import requests
+from urllib.parse import urlparse
+import pathlib
 
 from threatexchange.content_type.content_base import ContentType
 from threatexchange.content_type.photo import PhotoContent
@@ -35,18 +37,51 @@ async def hash(request: Request, url: str):
     if not is_valid_url(url):
         raise HTTPException(status_code=400, detail="Invalid or unsafe URL provided")
     
+    results = fetch_and_hash_url(url)
 
+    return { 'results': results }
+
+@router.post("/hash", response_model=HashResults)
+def hash_file(file: UploadFile):
+    results = []
+    if file.size is not None and file.size > settings.max_content_length:
+        raise ValueError("File content is too large")
+
+    content_type = get_content_type(file.content_type)
+    signal_types = get_signal_types(content_type)
+    
+    logger.info("Upload Error: %s is type %s", file.filename, content_type)
+
+    bytes = file.file.read()
+    for st in signal_types.values():
+        if issubclass(st, BytesHasher):
+            results.append({
+                'signal_name': st.get_name(),
+                'hash': st.hash_from_bytes(bytes)
+            })
+    
+    return { 'results': results }
+
+def fetch_and_hash_url(url: str) -> list[HashResult]:
     results = []
     # If content length is acceptable, proceed with GET request
     with requests.get(url, stream=True, timeout=30, allow_redirects=True) as response:
-        response.raise_for_status()
+        if response.status_code != 200:
+            logger.info("Fetch Error: %s returned %i", url, response.status_code)
+            return results
 
         # Double check content length from GET response
         content_length = response.headers.get("content-length")
+        if content_length is None or content_length == "0":
+            logger.info("Fetch Error: %s returned %i with zero content", url, response.status_code)
+            raise HTTPException(status_code=413, detail="Requested file return no content")
+
         if content_length is not None and int(content_length) > settings.max_content_length:
+            logger.info("Fetch Error: %s returned %i with too much content", url, response.status_code)
             raise HTTPException(status_code=413, detail="Requested file is too large")
-        
-        content_type = get_content_type(response.headers.get("content-type"), remote=True)
+
+        remote_file_extension = pathlib.Path(urlparse(response.url).path).suffix
+        content_type = get_content_type(response.headers.get("content-type"), remote_file_extension, remote=True)
         signal_types = get_signal_types(content_type)
         logger.info("%s is type %s", url, content_type)
 
@@ -71,52 +106,39 @@ async def hash(request: Request, url: str):
                             'signal_name': st.get_name(),
                             'hash': st.hash_from_file(path)
                         })
-        
-    return { 'results': results }
+    return results
 
-@router.post("/hash", response_model=HashResults)
-def hash_file(file: UploadFile):
-    results = []
-    if file.size is not None and file.size > settings.max_content_length:
-        raise ValueError("File content is too large")
-
-    content_type = get_content_type(file.content_type)
-    signal_types = get_signal_types(content_type)
-    
-    logger.info("%s is type %s", file.filename, content_type)
-
-    bytes = file.file.read()
-    for st in signal_types.values():
-        if issubclass(st, BytesHasher):
-            results.append({
-                'signal_name': st.get_name(),
-                'hash': st.hash_from_bytes(bytes)
-            })
-    
-    return { 'results': results }
-
-def get_content_type(content_type: str, remote: bool = False) -> t.Type[ContentType]:
+def get_content_type(content_type: str | None, remote_file_extension: str | None = None, remote: bool = False) -> t.Type[ContentType]:
     content_type_configs = get_storage().get_content_type_configs()
-    config: ContentTypeConfig
+    config: ContentTypeConfig | None = None
 
-    if content_type.lower().startswith("image"):
-        config = content_type_configs.get(PhotoContent.get_name())
-    elif content_type.lower().startswith("video") or content_type.lower() == 'application/octet-stream':
-        config = content_type_configs.get(VideoContent.get_name())
-    else:
-        raise RequestValidationError(
-            errors=(
-                ValidationError.from_exception_data(
-                    "ValueError",
-                    [
-                        InitErrorDetails(
-                            type=PydanticCustomError("value_error", "Unsupported content-type"),
-                            loc=("query", "url", "response", "headers", "content-type") if remote else ("body", "file", "content-type"),
-                        )
-                    ],
-                )
-            ).errors()
-        )
+    logger.info("%s extension: %s", content_type, remote_file_extension)
+
+    if content_type is not None:
+        if content_type.lower().startswith("image"):
+            config = content_type_configs.get(PhotoContent.get_name())
+        elif content_type.lower().startswith("video"):
+            config = content_type_configs.get(VideoContent.get_name())
+    
+    if config is None:
+        if remote_file_extension in ['.png', '.jpg', '.jpeg']:
+            config = content_type_configs.get(PhotoContent.get_name())
+        elif remote_file_extension in ['.mp4', '.mov']:
+            config = content_type_configs.get(VideoContent.get_name())
+        else:
+            raise RequestValidationError(
+                errors=(
+                    ValidationError.from_exception_data(
+                        "ValueError",
+                        [
+                            InitErrorDetails(
+                                type=PydanticCustomError("value_error", "Unsupported content-type"),
+                                loc=("query", "url", "response", "headers", "content-type") if remote else ("body", "file", "content-type"),
+                            )
+                        ],
+                    )
+                ).errors()
+            )
     
     if config is None:
         raise HTTPException(400, 'Unknown content type')
